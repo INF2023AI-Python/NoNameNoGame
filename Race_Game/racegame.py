b27842c7ada337498bb838b7c3c885173b1d172d
Tim Hirschmiller <156847281+Hiiirsch@users.noreply.github.com>
2024-03-26 00:32:01 +0100

    Kommentieren und löschen unnötiger Teile

--- Race_Game/racegame.py
+++ Race_Game/racegame.py
@@ -5,8 +5,11 @@ import os
 from utils import scale_image
 from utils import blit_rotate_center
 
+#Initalization pygame, joystick and Sound
 pygame.init()
 pygame.joystick.init()
+pygame.mixer.init()
+
 if pygame.joystick.get_count() == 0:
     print("Kein Gamepad gefunden.")
 
@@ -23,7 +26,7 @@ os.chdir(python_file_directory)
 font30 = pygame.font.SysFont('Constantia', 30)
 font40 = pygame.font.SysFont('Constantia', 40)
 
-# Laden der Bilder
+#Loading pictures
 FLAG = pygame.image.load("Graphics/racingflag.png")
 GRASS = pygame.image.load("Graphics/green_background.png")
 TRACK = scale_image(pygame.image.load("Graphics/racetrack.png"), 0.9)
@@ -35,30 +38,32 @@ STARTBOX1 = pygame.image.load("Graphics/startposition.png")
 STARTBOX1_POSITION = (580, 428)
 STARTBOX2 = pygame.image.load("Graphics/startposition.png")
 STARTBOX2_POSITION = (550, 475)
-# Autos
+
+#car
 RED_CAR = scale_image(pygame.image.load("Graphics/car_red.png"), 0.04)
 
-MAX_SPEED = 4
-MAX_ROTATION = 6
-pygame.mixer.init()
+#loading sounds
 ENGINE_SOUND = pygame.mixer.Sound("Sounds/car_sound.mp3")
 CRASH_SOUND = pygame.mixer.Sound("Sounds/crash_sound.mp3")
 
-start_cooldown = 0.5
-sound_playing = False
-collision_cooldown = 2.0  # Zeit in Sekunden für den Cooldown nach Kollision
-last_collision_time = 0.0  # Zeitstempel der letzten Kollision
-
+#Constants
+MAX_SPEED = 4
+MAX_ROTATION = 6
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
+collision_cooldown = 2.0  
+last_collision_time = 0.0  
+sound_playing = False
+FPS = 60
+
+#Set up screen and caption
 WIN = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("Racing Game")
 
-FPS = 60
 
-class AbstractCar:
+class AbstractCar: #for all cars, necessary for multiplayer, etc.
     IMG = RED_CAR
 
-    def __init__(self, max_vel, rotation_vel):
+    def __init__(self, max_vel, rotation_vel): #Initalization of the different variables
         self.img = self.IMG
         self.max_vel = max_vel
         self.vel = 0
@@ -67,47 +72,46 @@ class AbstractCar:
         self.x, self.y = self.START_POS
         self.acceleration = 0.3
 
-    def rotate(self, left=False, right=False):
+    def rotate(self, left=False, right=False):  #Rotation car
         if left:
             self.angle += self.rotation_vel
         elif right:
             self.angle -= self.rotation_vel
 
-    def draw(self, win):
+    def draw(self, win):    #draw car
         blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
 
-    def move_forward(self):
+    def move_forward(self): #forward
         self.vel = min(self.vel + self.acceleration, self.max_vel)
         self.move()
 
-    def move_backward(self):
+    def move_backward(self): #backward
         self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
         self.move()
 
-    def move(self):
+    def move(self): #mathematical calculation for movement
         radians = math.radians(self.angle)
         vertical = math.cos(radians) * self.vel
         horizontal = math.sin(radians) * self.vel
-
         self.y -= vertical
         self.x -= horizontal
 
-    def collide(self, mask, x=0, y=0):
+    def collide(self, mask, x=0, y=0):  #collide
         car_mask = pygame.mask.from_surface(self.img)
         offset = (int(self.x - x), int(self.y - y))
         poi = mask.overlap(car_mask, offset)
         return poi
 
-    def reset(self):
+    def reset(self): #reset
         self.x, self.y = self.START_POS
         self.angle = -90
         self.vel = 0
 
-class PlayerCar(AbstractCar):
+class PlayerCar(AbstractCar): #only for the red car
     IMG = RED_CAR
     START_POS = (549, 416)
 
-    def __init__(self, max_vel, rotation_vel):
+    def __init__(self, max_vel, rotation_vel): #Initalization of variables
         super().__init__(max_vel, rotation_vel)
         self.sound_playing = False
         self.max_sound_volume = 0.1
@@ -117,11 +121,11 @@ class PlayerCar(AbstractCar):
         self.start_time = 0
         self.end_time = 0
 
-    def reduce_speed(self):
+    def reduce_speed(self): #Reduce speed
         self.vel = max(self.vel - self.acceleration/2, 0)
         self.move()
 
-    def bounce(self):
+    def bounce(self): #bounce from boarder
         global last_collision_time
         if time.time() - last_collision_time > collision_cooldown:
             CRASH_SOUND.play()
@@ -129,7 +133,7 @@ class PlayerCar(AbstractCar):
         self.vel = -self.vel
         self.move()
 
-    def sound_car(self):
+    def sound_car(self): #carsound
         global sound_playing
         if self.vel != 0 and not self.sound_playing:
             ENGINE_SOUND.play(-1)
@@ -140,42 +144,44 @@ class PlayerCar(AbstractCar):
         volume = self.min_sound_volume + volume_factor * self.volume_range
         ENGINE_SOUND.set_volume(volume)
 
-def draw(win, images, player_car, lap_time):
+def draw(win, images, player_car, lap_time): #draw function for images and car
     for img, pos in images:
         win.blit(img, pos)
 
     player_car.draw(win)
-    # Rendern des Textes auf schwarzen Hintergrund
+    
+    #Rendering Text + Background
     lap_text_surface = font30.render("Rundenzeit: {:.2f}s".format(lap_time), True, (255, 255, 255))
     lap_background_surface = pygame.Surface((lap_text_surface.get_width(), lap_text_surface.get_height()))
     lap_background_surface.fill((200,200,200))
     lap_background_surface.blit(lap_text_surface, (0, 0))
     
-    # Positionieren der Textfläche
+    #Position text
     lap_rect = lap_background_surface.get_rect(center=(WIDTH // 2, 20))
     
-    # Zeichnen des Textes
+    #draw text
     win.blit(lap_background_surface, lap_rect)
     pygame.display.update()
 
+#variables
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0)), (FINISH, FINISH_POSITION),(STARTBOX1, STARTBOX1_POSITION),(STARTBOX1, STARTBOX2_POSITION), (TRACK_BOARDER, (0, 0))]
 player_car = PlayerCar(MAX_SPEED, MAX_ROTATION)
 lap_start_time = 0
-lap_started = False  # Initialisierung der Variable
-first_lap_started = False  # Variable, um festzustellen, ob die erste Runde begonnen hat
-previous_lap_time = 0  # Variable zur Speicherung der vorherigen Rundenzeit
+lap_started = False  
+first_lap_started = False  #Variable to determine whether the first round has started
 round_cooldown = False
 
-countdown = 3  # Initialisierung des Countdowns
+countdown = 3  #Initalization countdown start
 
+#loop
 while run:
     clock.tick(FPS)
 
     elapsed_time = 0
 
-    # Ereignisschleife
+    #Eventloop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -184,40 +190,43 @@ while run:
             if event.key == pygame.K_x:
                 player_car.reset()
         
-    # Countdown wird nur gemalt, wenn noch nicht abgelaufen
+    #loading screen   
     if countdown > 0:
-        WIN.blit(FLAG, (0,0)) # Schwarzer Hintergrund für den Countdown
+        WIN.blit(FLAG, (0,0)) 
         countdown_surface = font40.render("RACEGAME", True, (255, 255, 255))
         countdown_rect = countdown_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2))
         WIN.blit(countdown_surface, countdown_rect)
         pygame.display.update()
-        time.sleep(1)  # Eine Sekunde warten
-        countdown -= 1  # Countdown herunterzählen
-        continue  # Den Rest des Haupt-Loops überspringen, während der Countdown läuft
+        time.sleep(1)  
+        countdown -= 1  
+        continue  
         
-    # Steuerung des Autos nur nach Countdown-Ende
-    if joystick.get_axis(0) < -0.9:
+    #Steering the car only after countdown
+    #control with joystick
+
+    if joystick.get_axis(0) < -0.9:#right
         player_car.rotate(right=True)
-    elif joystick.get_axis(0) > 0.9:
+    elif joystick.get_axis(0) > 0.9:#left
         player_car.rotate(left=True)
         
-    if joystick.get_button(3):
+    if joystick.get_button(3):#forward
         moved = True
         player_car.move_forward()
         
-    if joystick.get_button(4):
+    if joystick.get_button(4):#backward
         moved = True
         player_car.move_backward()
 
-    if joystick.get_button(1):
+    if joystick.get_button(1):#reset
         player_car.reset()
 
-    if joystick.get_button(0):
+    if joystick.get_button(0):#quit
         pygame.quit()
     
     keys = pygame.key.get_pressed()
     moved = False
 
+    #control with keyboard
     if keys[pygame.K_a]:
         player_car.rotate(left=True)
     if keys[pygame.K_d]:
@@ -234,11 +243,11 @@ while run:
         moved = False
 
     
-    # Kollision mit der Streckenbegrenzung
+    #collision with boarder
     if player_car.collide(TRACK_BOARDER_MASK) is not None:
         player_car.bounce()
 
-
+    #logic for timekeeping
     if player_car.collide(pygame.mask.from_surface(FINISH), FINISH_POSITION[0], FINISH_POSITION[1]):
         if not player_car.finished:
             player_car.finished = True
@@ -254,18 +263,17 @@ while run:
     if player_car.collide(pygame.mask.from_surface(FINISH), FINISH_POSITION[0], FINISH_POSITION[1]) and not lap_started:
         lap_start_time = time.time()
         lap_started = True
-        first_lap_started = True  # Die erste Runde hat begonnen
+        first_lap_started = True  
 
 
     if first_lap_started:  
         draw(WIN, images, player_car, time.time() - lap_start_time)
-        lap_surface = font30.render("Vorherige Rundenzeit: {:.2f}s".format(previous_lap_time), True, (255, 255, 255))
-        WIN.blit(lap_surface, (10, 60))
     else:
-        draw(WIN, images, player_car, 0)  # Zeit auf 0 setzen, wenn die erste Runde noch nicht begonnen hat
+        draw(WIN, images, player_car, 0)  
 
     player_car.sound_car()
 
-    print("X-Koordinate:", player_car.x)
-    print("Y-Koordinate:", player_car.y)
+    #only for testing necessary
+    #print("X-Koordinate:", player_car.x)
+    #print("Y-Koordinate:", player_car.y)
 pygame.quit()
