332b29cbb2805aaba2b4a79888573dd1adb96325
GeroldJ <157789601+GeroldJ@users.noreply.github.com>
2024-03-25 14:28:05 +0100

    added restart and close button in spaceinvaders

--- Space_Invaders/Spaceinvaders.py
+++ Space_Invaders/Spaceinvaders.py
@@ -252,6 +252,8 @@ while running:
             bullet = Bullets(spaceship.rect.centerx, spaceship.rect.top)
             bullet_group.add(bullet)
         elif event.type == pygame.JOYBUTTONDOWN and event.button == 0:
+             mainMenu
+        elif event.type == pygame.JOYBUTTONDOWN and event.button == 1:
              restart()
         elif event.type == JOYAXISMOTION:
                 axis = event.axis
